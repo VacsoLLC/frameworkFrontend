07341d86acf14917e941f242dd18503febe5708d
BhuvaneshPatil <bhuvaneshpatil80642@gmail.com>
2024-09-29 21:53:13 +0530

    updated log in with

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -148,13 +148,17 @@ export default function LoginModal({children}) {
                 Login
               </Button>
             </div>
-
-            <Separator className="my-6" />
-
+            <div className="relative my-4">
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t" />
+              </div>
+              <div className="relative flex justify-center text-xs uppercase">
+                <span className="bg-background px-2 text-muted-foreground">
+                  Or continue with
+                </span>
+              </div>
+            </div>
             <div className="space-y-4">
-              <p className="text-sm text-center text-muted-foreground">
-                Or login with...
-              </p>
               {ssoList?.length &&
                 ssoList.map((sso) => (
                   <Button
