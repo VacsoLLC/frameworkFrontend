b39d97917813a6edac8c7e069cb351cf657ffdc7
Tripp Donnelly <tripp@wasteoftime.org>
2024-11-08 14:45:35 -0600

    export user store

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ import {
 } from './src/lib/usebackend.js';
 
 import ActionButton from './src/components/buttons/actionbutton.jsx';
+import useUserStore from './src/stores/user.js';
 
 export default FrontEnd;
-export {useBackend, callBackend, api, clearCache, FrontEnd, ActionButton};
+export {
+  useBackend,
+  callBackend,
+  api,
+  clearCache,
+  FrontEnd,
+  ActionButton,
+  useUserStore,
+};
