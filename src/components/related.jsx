c1b2205ad1c94c1c3f6746c76f5f92dc2fdd4c03
Tripp Donnelly <tripp@wasteoftime.org>
2024-12-28 15:17:56 -0600

    bugfix

--- src/components/related.jsx
+++ src/components/related.jsx
@@ -91,10 +91,8 @@ function prepTables({tablesRaw, db, table, recordId}) {
   const tablesTemp = [...tablesRaw.data]; // copy the cached response since we're going to modify it.
 
   for (const childTable of tablesTemp) {
+    childTable.where = [];
     for (const [columna, columnb] of Object.entries(childTable.columnmap)) {
-      if (!childTable.where) {
-        childTable.where = [];
-      }
       let right = '';
       switch (columnb) {
         case 'id':
